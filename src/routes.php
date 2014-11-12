cad61969ee430743e5fec0a3c757060bbbf2a2ab
Mark Cameron <mark.cameron@devfactory.ch>
2014-11-12 15:28:44 +0100

    Modified routes.php to use a route group and a ressource controller

--- src/routes.php
+++ src/routes.php
@@ -2,13 +2,8 @@
 
 $prefix = Config::get('taxonomy::route_prefix');
 
-#voc management
-Route::get($prefix.'/vocabularies', 'Devfactory\Taxonomy\VocabulariesController@index');
-Route::get($prefix.'/vocabularies/create', 'Devfactory\Taxonomy\VocabulariesController@create');
-Route::post($prefix.'/vocabularies/create', 'Devfactory\Taxonomy\VocabulariesController@store');
-Route::get($prefix.'/vocabularies/show/{id}', 'Devfactory\Taxonomy\VocabulariesController@show');
-Route::get($prefix.'/vocabularies/delete/{id}', 'Devfactory\Taxonomy\VocabulariesController@destroy');
+Route::group(array('prefix' => 'admin'), function() {
 
-Route::get($prefix.'/vocabularies/edit/{id}', 'Devfactory\Taxonomy\VocabulariesController@edit');
-Route::post($prefix.'/vocabularies/edit/{id}', 'Devfactory\Taxonomy\VocabulariesController@update');
+  Route::resource('taxonomy', 'Devfactory\Taxonomy\Controllers\TaxonomyController');
 
+});
