254d63b37cfe4735d9e4277633450eac554fc511
Sean Fisher <srtfisher@gmail.com>
2015-03-30 19:42:25 -0400

    Renaming variable and adding in before filter

--- src/config/config.php
+++ src/config/config.php
@@ -1,11 +1,17 @@
 <?php
 
-return array(
-	'admin_interface' => true,
+return [
 	'route_prefix' => 'admin',
+
+	// Before filter
+	'route_before' => [],
+
+	// Enabled the use of the admin interace
+	'route_enabled' => true,
+
   'layout' => [
     'extends' => 'layouts.admin',
     'header' => 'header',
     'content' => 'content',
   ],
-);
+];
--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,10 @@
 <?php
-$admin_interface = (bool) Config::get('taxonomy::admin_interface', true);
-if ($admin_interface) {
+$route_enabled = (bool) Config::get('taxonomy::route_enabled', true);
+if ($route_enabled) {
   $prefix = Config::get('taxonomy::route_prefix');
+  $route_before = Config::get('taxonomy::route_before', []);
 
-  Route::group(array('prefix' => $prefix), function() use ($prefix) {
+  Route::group(array('prefix' => $prefix, 'before' => $route_before), function() use ($prefix) {
 
     Route::resource('taxonomy', 'Devfactory\Taxonomy\Controllers\TaxonomyController');
 
