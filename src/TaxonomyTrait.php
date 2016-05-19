d38439304bd8d49e2ef7d185bcb54b78cd3d4649
Will Murray <willdavidmurray@gmail.com>
2016-05-19 11:47:55 +0100

    Tabs to spaces

--- src/TaxonomyTrait.php
+++ src/TaxonomyTrait.php
@@ -6,13 +6,13 @@ use Devfactory\Taxonomy\Models\Term;
 trait TaxonomyTrait {
 
   /**
-	 * Return collection of tags related to the tagged model
-	 *
-	 * @return Illuminate\Database\Eloquent\Collection
-	 */
-	public function related() {
-		return $this->morphMany('Devfactory\Taxonomy\Models\TermRelation', 'relationable');
-	}
+     * Return collection of tags related to the tagged model
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function related() {
+        return $this->morphMany('Devfactory\Taxonomy\Models\TermRelation', 'relationable');
+    }
 
   /**
    * Add an existing term to the inheriting model
